6ac1ba7bc2540810c8087083b9c0acda41dc7c18
DerZerstampfer <51781549+DerZerstampfer@users.noreply.github.com>
2022-07-05 13:17:35 +0200

    new return type for getWinner

--- index.ts
+++ index.ts
@@ -3,25 +3,11 @@ import { generateDeck, getHandsFromDeck, getWinner } from "./poker";
 async function main() {
   let deck = generateDeck();
   let hands = getHandsFromDeck(deck, 2);
+  let winner = getWinner(hands);
 
-  const hands2 = [
-    [
-      { value: "J", suit: "S" },
-      { value: "Q", suit: "C" },
-      { value: "A", suit: "C" },
-      { value: "J", suit: "C" },
-      { value: "A", suit: "H" },
-    ],
-    [
-      { value: "3", suit: "S" },
-      { value: "J", suit: "D" },
-      { value: "9", suit: "S" },
-      { value: "J", suit: "H" },
-      { value: "9", suit: "S" },
-    ],
-  ];
-
-  console.log(`Hand ${getWinner(hands2)} hat gewonnen! (Starting from 0)`);
+  console.log(hands);
+  console.log(winner);
+  console.log(`Hand ${winner.index} hat gewonnen! (Starting from 0)`);
 }
 
 main();
